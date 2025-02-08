70851ef94789aede70b6fd7bdae867f9754e525a
umarthe1st <umarrashad2006@gmail.com>
2025-02-09 00:13:58 +0800

    testing

--- index.js
+++ index.js
@@ -1,22 +1,48 @@
-var express = require('express');
-var bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require("body-parser");
 const logger = require('./logger');
-var app = express();
+const promClient = require('prom-client');
 
-const PORT = process.env.PORT || 5050
-var startPage = "index.html";
+const app = express();
+const PORT = process.env.PORT || 5050;
 
+// Create a Registry for Prometheus metrics
+const register = new promClient.Registry();
+promClient.collectDefaultMetrics({ register });
+
+// Fix: Register default metrics under the correct register
+const httpRequestCounter = new promClient.Counter({
+    name: 'http_requests_total',
+    help: 'Total number of HTTP requests',
+    labelNames: ['method', 'route', 'status'],
+});
+register.registerMetric(httpRequestCounter); // Important: Register the metric
+
+// Middleware to track requests
+app.use((req, res, next) => {
+    res.on('finish', () => {
+        httpRequestCounter.labels(req.method, req.path, res.statusCode).inc();
+    });
+    next();
+});
+
+// Fix: Correctly expose the `/metrics` endpoint
+app.get('/metrics', async (req, res) => {
+    res.set('Content-Type', register.contentType);
+    res.send(await register.metrics()); // Fix: Use `send()` instead of `end()`
+});
+
+// Fix: Use middleware after defining metrics
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("./public"));
 app.use(express.static("./utils"));
 
-const { addMenuItem, viewMenu } = require('./utils/MenuUtil.js')
+const { addMenuItem, viewMenu } = require('./utils/MenuUtil.js');
 app.post('/add-MenuItem', addMenuItem);
-app.get('/view-menu', viewMenu)
-const { createReservation, viewReservation,  } = require('./utils/Reservationutils.js');
-const {  editReservation, deleteReservation, } = require('./utils/UpdateUtil.js');
-
+app.get('/view-menu', viewMenu);
+const { createReservation, viewReservation } = require('./utils/Reservationutils.js');
+const { editReservation, deleteReservation } = require('./utils/UpdateUtil.js');
 
 app.post('/reservation', createReservation);
 app.get('/view-reservation', viewReservation);
@@ -24,18 +50,17 @@ app.put('/edit-reservation/:id', editReservation);
 app.delete('/delete-reservation/:id', deleteReservation);
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/public/" + startPage);
+    res.sendFile(__dirname + "/public/index.html");
 });
 
+// Add Express Status Monitor
 const statusMonitor = require('express-status-monitor');
-app.use(statusMonitor())
+app.use(statusMonitor());
 
-server = app.listen(PORT, function () {
-    const address = server.address();
-    const baseUrl = `http://${address.address == "::" ? 'localhost' : address.address}:${address.port}`;
-    console.log(`Demo project at: ${baseUrl}`);
-    logger.info(`Demo project at: ${baseUrl}!`);
-    logger.error(`Example or error log`)
+// Start Express server
+server = app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+    logger.info(`Server running at http://localhost:${PORT}!`);
 });
 
 module.exports = { app, server };
